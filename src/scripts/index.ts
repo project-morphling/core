7cfca7503f87100fcdd5d1eca9d4a0720888a74d
cypherix93 <bikram.ghosh93@gmail.com>
2018-02-26 00:08:28 -0500

    Fixed vue bootstrap.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,8 +1,16 @@
 import './vendor';
 
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+
 import {mount} from './routing/mount';
 
 import App from '../components/App.vue';
+
+// Init Vue Native Modules
+Vue.use(Vuex);
+Vue.use(VueRouter);
 
 // Mount Vue
 mount(`#app-container`, App);
