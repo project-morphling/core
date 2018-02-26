33bdd4d1ab17356d2d06747eb0866bf36abd9e41
cypherix93 <bikram.ghosh93@gmail.com>
2018-02-26 00:08:34 -0500

    Better boilerplate.

--- src/components/layout/header/Header.vue
+++ src/components/layout/header/Header.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="container-fluid">
-        <img class="img-responsive" src="http://via.placeholder.com/1920x300">
+        <img class="img-fluid" src="http://via.placeholder.com/1920x300">
     </div>
 </template>
--- src/components/pages/home/Home.vue
+++ src/components/pages/home/Home.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="jumbotron">
-        <h1>Hello World!</h1>
-
-        <hr>
-
-        <h3>How is it going? Glad to see you are here.</h3>
+    <div class="container mt-4">
+        <div class="card bg-light">
+            <div class="card-body">
+                <h1>Hello World!</h1>
+                <hr>
+                <h3>How is it going? Glad to see you are here.</h3>
+            </div>
+        </div>
     </div>
 </template>
